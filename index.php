ad99aa915fef7e6355f3db0f4bf93d9454ac5784
Yianm24 <yancamacaro@gmail.com>
2026-03-25 18:12:26 -0400

    Edicion de codigo en index para  estilos

--- index.php
+++ index.php
@@ -1,22 +1,35 @@
 <?php
-  //require_once "class/conex.php";
-  require_once "class/cliente.php";
-  require_once "class/repuesto.php";
-  require_once "class/vehiculo.php";
- 
+//require_once "class/conex.php";
+require_once "class/cliente.php";
+require_once "class/repuesto.php";
+require_once "class/vehiculo.php";
+
 ?>
 
 <!DOCTYPE html>
 <html lang="es">
-  <?php 
-    include 'view/head.php';
-    include 'view/header.php';
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <link rel="stylesheet" href="estilos/bootstrap.min.css">
+  <!--<link rel="stylesheet" href="./css/style.css">-->
+  <!--<link rel="stylesheet" href="./style.css">-->
+  <!--<link rel="stylesheet" href="class/style.css">-->
+  <link rel="stylesheet" href="estilos/stylesnoBootstrap.css">
+  <title>TallerMecanico</title>
+
+</head>
+
+<body>
+  <?php
+  include 'view/header.php';
   ?>
-  <body>
-    <?php 
-      require_once "view/clienteForm.php";
-      require_once "view/repuestoForm.php";
-      require_once "view/vehiculoForm.php";
-    ?>
-  </body>
+  <h1>Taller Mecanico: Las Roscas C.A</h1>
+  <?php
+  require_once "view/clienteForm.php";
+  require_once "view/repuestoForm.php";
+  require_once "view/vehiculoForm.php";
+  ?>
+</body>
+
 </html>
