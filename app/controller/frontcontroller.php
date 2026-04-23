7f7d88f281ce66d3008a0c5399a1a7ae98a7245e
HoneyMilu <malau200104@gmail.com>
2026-04-23 10:47:29 -0400

    correcciones de estilo

--- app/controller/frontcontroller.php
+++ app/controller/frontcontroller.php
@@ -12,7 +12,8 @@ require_once "app/model/vehiculo.php";
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href='assets\bootstrap\bootstrap.min.css'>
-  <link rel="stylesheet" href='assets\css\styles.css'>
+  <link rel="stylesheet" href="assets/css/styles.css?v=<?php echo time(); ?>">
+  
   <title>TallerMecanico</title>
 
 </head>
